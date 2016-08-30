7f73cde49d962f959243c05faffa2dd4956b26b1
rb1whitney <rb1whitney@msn.com>
2016-08-29 21:47:07 -0600

    cleaning up files

---
+++ cookbooks/appserver/attributes/default.rb
@@ -0,0 +1,2 @@
+default['appserver']['log_name']='rotate_logs'
+default['appserver']['log_paths']=['/cust/logs/*/*']
--- cookbooks/custom_recipes/recipes/logrotate.rb
+++
@@ -1,9 +0,0 @@
-include_recipe 'logrotate'
-
-logrotate_app node['logrotate']['log_name'] do
-  cookbook 'logrotate'
-  path node['appserver']['log_paths']
-  frequency 'daily'
-  create '644 root adm'
-  rotate 7
-end
